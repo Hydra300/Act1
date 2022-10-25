1111fb91bb687dc3e4d3a42e5999766016e8210f
Hydra300 <ralphgarcia360@gmail.com>
2022-10-26 01:22:12 +0800

    act 1

---
+++ app/src/main/java/com/example/gridlayout/MainL.java
@@ -0,0 +1,15 @@
+package com.example.gridlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainL extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+    }
+}
